d078ee8efb39587f0300269a1da22bfc0954e010
George Singer <george.w.singer@gmail.com>
2019-01-20 10:23:09 -0500

    Nixified repo.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
+
+pkgs.haskell.lib.buildStackProject {
+  name = "simula-godot";
+  inherit ghc;
+  buildInputs = with pkgs; [
+                          #  libinput  # <-- Possibly too recent for hsroots
+                             (callPackage ./nix/libinput/default.nix { } )
+                             libxkbcommon
+                             pkgconfig
+                             (callPackage ./nix/wayland.nix { } )
+                             (callPackage ./nix/wayland-protocols.nix { } )
+                             (callPackage ./nix/wlroots.nix { } )
+                             xorg.pixman
+                          ];
+
+  LANG = "en_US.UTF-8";
+  TMPDIR = "/tmp";
+}
