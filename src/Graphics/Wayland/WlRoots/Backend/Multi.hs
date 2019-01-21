92adff771783f4b4f36754d26109463daca8c2e6
George Singer <george.w.singer@gmail.com>
2019-01-21 16:28:29 -0500

    Removed Multi.hs and Tabletv2.hsc due to their outdated wlroots calls.

--- src/Graphics/Wayland/WlRoots/Backend/Multi.hs
+++ src/Graphics/Wayland/WlRoots/Backend/Multi.hs
@@ -1,40 +1,41 @@
 module Graphics.Wayland.WlRoots.Backend.Multi
-    ( getSession
-    , isMulti
+    (
+    --   getSession
+    -- , isMulti
 
-    , getSession'
-    , addBackend
+    -- , getSession'
+    -- , addBackend
     )
 where
 
-import Data.Word (Word8)
-import Foreign.C.Error (throwErrnoIfNull)
-import Foreign.Ptr (Ptr)
+-- import Data.Word (Word8)
+-- import Foreign.C.Error (throwErrnoIfNull)
+-- import Foreign.Ptr (Ptr)
 
-import Graphics.Wayland.WlRoots.Backend.Session (WlrSession)
-import Graphics.Wayland.WlRoots.Backend (Backend)
-
-
-foreign import ccall unsafe "wlr_backend_is_multi" c_is_multi :: Ptr Backend -> IO Word8
-
-isMulti :: Ptr Backend -> IO Bool
-isMulti = fmap (/= 0) . c_is_multi
+-- import Graphics.Wayland.WlRoots.Backend.Session (WlrSession)
+-- import Graphics.Wayland.WlRoots.Backend (Backend)
 
 
-foreign import ccall unsafe "wlr_multi_get_session" c_get_session :: Ptr Backend -> IO (Ptr WlrSession)
+-- foreign import ccall unsafe "wlr_backend_is_multi" c_is_multi :: Ptr Backend -> IO Word8
 
-getSession :: Ptr Backend -> IO (Ptr WlrSession)
-getSession = throwErrnoIfNull "getSession" . c_get_session
+-- isMulti :: Ptr Backend -> IO Bool
+-- isMulti = fmap (/= 0) . c_is_multi
 
 
-getSession' :: Ptr Backend -> IO (Maybe (Ptr WlrSession))
-getSession' backend = do
-    multi <- isMulti backend
-    if multi
-       then Just <$> getSession backend
-       else pure Nothing
+-- foreign import ccall unsafe "wlr_multi_get_session" c_get_session :: Ptr Backend -> IO (Ptr WlrSession)
 
-foreign import ccall "wlr_multi_backend_add" c_multi_add :: Ptr Backend -> Ptr Backend -> IO ()
+-- getSession :: Ptr Backend -> IO (Ptr WlrSession)
+-- getSession = throwErrnoIfNull "getSession" . c_get_session
 
-addBackend :: Ptr Backend -> Ptr Backend -> IO ()
-addBackend = c_multi_add
+
+-- getSession' :: Ptr Backend -> IO (Maybe (Ptr WlrSession))
+-- getSession' backend = do
+--     multi <- isMulti backend
+--     if multi
+--        then Just <$> getSession backend
+--        else pure Nothing
+
+-- foreign import ccall "wlr_multi_backend_add" c_multi_add :: Ptr Backend -> Ptr Backend -> IO ()
+
+-- addBackend :: Ptr Backend -> Ptr Backend -> IO ()
+-- addBackend = c_multi_add
